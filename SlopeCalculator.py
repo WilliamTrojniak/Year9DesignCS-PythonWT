bdba45722b11ee914d4888e3efe7a4974331f457
William Trojniak <william.trojniak@Y22-Design-William-Trojniak.local>
2018-09-17 15:24:03 -0400

    Slope Calculator Updated to take input and output slope

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,2 +1,19 @@
-println("Slope Calculator")
+print("Slope Calculator")
 
+#Input
+#Point 1
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+#Point 2
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+#Process
+#Calculate slope
+rise = (y2 - y1) 
+run = (x2 - x1)
+m = rise/run
+#Output
+#Print the slope
+print("The slope of your line is: ", rise, "/", run)
+print("Or: ", m)
