2a95d57d8b1b4dc98b59bd6268bef7030e5a3e45
William Trojniak <william.trojniak@Y22-Design-William-Trojniak.local>
2018-10-01 10:45:56 -0400

    Added more examples of uses for loops

--- LoopDemo.py
+++ LoopDemo.py
@@ -18,6 +18,38 @@ for i in range(0, 5, 1):
 	print(i+1)
 
 print("*********************")
+print("7 to 104, odd numbers inclusive")
+for i in range(7, 104, 2):
+	print(i)
+
+print("*********************")
+print("-22 to 134, even numbers inclusive")
+for i in range(-22, 135, 2):
+	print(i)
+
+print("*********************")
+print("Count down from 100 to 0 by 2")
+for i in range(100, -1, -2):
+	print(i)
+
+print("*********************")
+print("Count down from 101 to 0 inclusive")
+for i in range(101, -1, -1):
+	print(i)
+
+print("*********************")
+s = "Yaba Daba Dooo!"
+print("Every character of string")
+for i in range((len(s)-1), -1, -1):
+	print(s[i])
+
+print("*********************")
+s = "Yaba Daba Dooo!"
+print("Every other character of string")
+for i in range(0, len(s), 2):
+	print(s[i])
+
+print("*********************")
 
 print("Conditional Loop")
 #Conditional Loop
