3deffd512e66eac27f8f319b3349bc4090259c31
William Trojniak <william.trojniak@Y22-Design-William-Trojniak.local>
2018-11-20 18:27:15 -0500

    Started to add asthetic customization within code

--- Project1/ProjectWT.py
+++ Project1/ProjectWT.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font as tkFont
 
 class Display:
 	def __init__(self):
@@ -6,31 +7,41 @@ class Display:
 		self.root = tk.Tk()
 
 		#variables
-		self.genre = tk.StringVar(self.root)
+		self.genre = tk.StringVar()
 		self.genre.set("Select Option")
 		self.genreOptions = ["Action", "Fantasy", "Romance"]
 		self.gender = tk.StringVar()
 		self.title = "STORY GENERATOR"
 		self.maxTrials = 3
 		self.minTrials = 1
+		self.titleFont = tkFont.Font(family = "Times", size = 22, weight = "bold", underline = 1)
 		self.maxFontSize = 100
 		self.minFontSize = 10
-		self.fontSize = 12
+		self.guiLblFont = tkFont.Font(family = "Helvitica", size = 12, weight = "normal")
+		self.guiFrameFont = tkFont.Font(family = "Helvitica", size = 14, weight = "bold")
 		self.enableHighContrast = 0
+		self.frameBckgrndClr = "grey"
+		self.lblBgrndClr = "grey"
+		self.entrBgrndClr = "lightgrey"
+		self.framePadY = 1
+		self.framePadX = 5
+		self.entrPadX = 2
+		self.entrPadY = 2
+
 
 #**** Widgets ******************************************************************************************************************************************
 
 
 		#Title
-		self.titleLabel = tk.Label(self.root, text = self.title, font = ("Helvetica", 16), background = "yellow", foreground = "blue")
+		self.titleLabel = tk.Label(self.root, text = self.title, font = self.titleFont, background = "yellow", foreground = "blue", pady = 0)
 		self.titleLabel.grid(row = 0, column = 0, columnspan = 3, sticky = "NESW")
 
 		#Story Settings Frame
-		self.storyFrame = tk.LabelFrame(self.root, text = "Story Settings")
-		self.genreLbl = tk.Label(self.storyFrame, text = "Select Genre")
-		self.strtPlaceLbl = tk.Label(self.storyFrame, text = "Starting Place")
+		self.storyFrame = tk.LabelFrame(self.root, text = "Story Settings", background = self.frameBckgrndClr, font = self.guiFrameFont, pady = self.framePadY, padx = self.framePadX)
+		self.genreLbl = tk.Label(self.storyFrame, text = "Select Genre", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.strtPlaceLbl = tk.Label(self.storyFrame, text = "Starting Place", background = self.lblBgrndClr, font = self.guiLblFont)
 		self.genreOMenu = tk.OptionMenu(self.storyFrame, self.genre, self.genreOptions[0], self.genreOptions[1], self.genreOptions[2])
-		self.strtPlaceEntr = tk.Entry(self.storyFrame)
+		self.strtPlaceEntr = tk.Entry(self.storyFrame, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.storyFrame.grid(row = 1, column = 1)
 		self.genreLbl.grid(row = 0, column = 0)
@@ -39,16 +50,16 @@ class Display:
 		self.strtPlaceEntr.grid(row = 1, column = 1)
 
 		#Hero's Characteristics Frame
-		self.heroFrame = tk.LabelFrame(self.root, text = "Hero's Characteristics")
-		self.heroNmLbl = tk.Label(self.heroFrame, text = "Hero's Name")
-		self.heroNmEntr = tk.Entry(self.heroFrame)
-		self.heroGenderLbl = tk.Label(self.heroFrame, text = "Hero's Gender")
-		self.heroStrngthLb  = tk.Label(self.heroFrame, text = "Hero's Strengths")
-		self.heroGenderMRBttn = tk.Radiobutton(self.heroFrame, text = "Male", variable = self.gender, value = "male")
-		self.heroGenderFRBttn = tk.Radiobutton(self.heroFrame, text = "Female", variable = self.gender, value = "female")
-		self.heroStrngthEntr1 = tk.Entry(self.heroFrame)
-		self.heroStrngthEntr2 = tk.Entry(self.heroFrame)
-		self.heroStrngthEntr3 = tk.Entry(self.heroFrame)
+		self.heroFrame = tk.LabelFrame(self.root, text = "Hero's Characteristics", background = self.frameBckgrndClr, font = self.guiFrameFont, padx = self.framePadX, pady = self.framePadY)
+		self.heroNmLbl = tk.Label(self.heroFrame, text = "Hero's Name", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.heroNmEntr = tk.Entry(self.heroFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.heroGenderLbl = tk.Label(self.heroFrame, text = "Hero's Gender", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.heroStrngthLb  = tk.Label(self.heroFrame, text = "Hero's Strengths", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.heroGenderMRBttn = tk.Radiobutton(self.heroFrame, text = "Male", variable = self.gender, value = "male", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.heroGenderFRBttn = tk.Radiobutton(self.heroFrame, text = "Female", variable = self.gender, value = "female", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.heroStrngthEntr1 = tk.Entry(self.heroFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.heroStrngthEntr2 = tk.Entry(self.heroFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.heroStrngthEntr3 = tk.Entry(self.heroFrame, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.heroFrame.grid(row = 2, column = 1)
 		self.heroNmLbl.grid(row = 0, column = 0, columnspan = 2)
@@ -57,16 +68,16 @@ class Display:
 		self.heroStrngthLb.grid(row = 0, column = 2, columnspan = 2)
 		self.heroGenderMRBttn.grid(row = 3, column = 0)
 		self.heroGenderFRBttn.grid(row = 3, column = 1)
-		self.heroStrngthEntr1.grid(row = 1, column = 2)
-		self.heroStrngthEntr2.grid(row = 2, column = 2)
-		self.heroStrngthEntr3.grid(row = 3, column = 2)
+		self.heroStrngthEntr1.grid(row = 1, column = 2, padx = self.entrPadX, pady = self.entrPadY)
+		self.heroStrngthEntr2.grid(row = 2, column = 2, padx = self.entrPadX, pady = self.entrPadY)
+		self.heroStrngthEntr3.grid(row = 3, column = 2, padx = self.entrPadX, pady = self.entrPadY)
 
 		#Mentor's Characteristics Frame
-		self.mntrFrame = tk.LabelFrame(self.root, text = "Mentor's Characteristics")
-		self.mntrNmLbl = tk.Label(self.mntrFrame, text = "Mentor's Name")
-		self.mntrNmEntr = tk.Entry(self.mntrFrame)
-		self.mntrGftLbl = tk.Label(self.mntrFrame, text ="Mentor's Gift")
-		self.mntrGftEntr = tk.Entry(self.mntrFrame)
+		self.mntrFrame = tk.LabelFrame(self.root, text = "Mentor's Characteristics", background = self.frameBckgrndClr, font = self.guiFrameFont, padx = self.framePadX, pady = self.framePadY)
+		self.mntrNmLbl = tk.Label(self.mntrFrame, text = "Mentor's Name", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.mntrNmEntr = tk.Entry(self.mntrFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.mntrGftLbl = tk.Label(self.mntrFrame, text ="Mentor's Gift", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.mntrGftEntr = tk.Entry(self.mntrFrame, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.mntrFrame.grid(row = 3, column = 1)
 		self.mntrNmLbl.grid(row = 0, column = 0)
@@ -75,13 +86,13 @@ class Display:
 		self.mntrGftEntr.grid(row = 1, column = 1)
 
 		#Trials Information
-		self.trialFrame = tk.LabelFrame(self.root, text = "Trials Information")	
-		self.trialCntLbl = tk.Label(self.trialFrame, text = "Number of Trials")
-		self.trialCntSpBx = tk.Spinbox(self.trialFrame, from_ = self.minTrials, to = self.maxTrials)
-		self.trialLocLbl = tk.Label(self.trialFrame, text = "Location of Trials")
-		self.trialLocEntr = tk.Entry(self.trialFrame)
-		self.trialNmLbl = tk.Label(self.trialFrame, text = "Trial")
-		self.trialNmEntr = tk.Entry(self.trialFrame)
+		self.trialFrame = tk.LabelFrame(self.root, text = "Trials Information", background = self.frameBckgrndClr, font = self.guiFrameFont, padx = self.framePadX, pady = self.framePadY)	
+		self.trialCntLbl = tk.Label(self.trialFrame, text = "Number of Trials", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.trialCntSpBx = tk.Spinbox(self.trialFrame, from_ = self.minTrials, to = self.maxTrials, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.trialLocLbl = tk.Label(self.trialFrame, text = "Location of Trial", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.trialLocEntr = tk.Entry(self.trialFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.trialNmLbl = tk.Label(self.trialFrame, text = "Trial", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.trialNmEntr = tk.Entry(self.trialFrame, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.trialFrame.grid(row = 4, column = 1)
 		self.trialCntLbl.grid(row = 0, column = 1, columnspan = 2)
@@ -92,11 +103,11 @@ class Display:
 		self.trialNmEntr.grid(row =3, column = 2, columnspan = 3)
 
 		#Final Challenge Information
-		self.fChllngeFrame = tk.LabelFrame(self.root, text = "Final Challenge Information")
-		self.fChllngeLocLbl = tk.Label(self.fChllngeFrame, text = "Location of Final Challenge")
-		self.fChllngeLocEntr = tk.Entry(self.fChllngeFrame)
-		self.fChllngeNmLbl = tk.Label(self.fChllngeFrame, text = "Final Challenge")
-		self.fChllngeNmEntr = tk.Entry(self.fChllngeFrame)
+		self.fChllngeFrame = tk.LabelFrame(self.root, text = "Final Challenge Information", background = self.frameBckgrndClr, font = self.guiFrameFont, padx = self.framePadX, pady = self.framePadY)
+		self.fChllngeLocLbl = tk.Label(self.fChllngeFrame, text = "Location of Final Challenge", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.fChllngeLocEntr = tk.Entry(self.fChllngeFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.fChllngeNmLbl = tk.Label(self.fChllngeFrame, text = "Final Challenge", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.fChllngeNmEntr = tk.Entry(self.fChllngeFrame, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.fChllngeFrame.grid(row = 5, column = 1)
 		self.fChllngeLocLbl.grid(row = 0, column = 0)
@@ -105,11 +116,11 @@ class Display:
 		self.fChllngeNmEntr.grid(row = 1, column = 1)
 
 		#End Stage Information
-		self.endFrame = tk.LabelFrame(self.root, text = "End Story")
-		self.trnsprtHmLbl = tk.Label(self.endFrame, text = "Method of Getting Home")
-		self.trnsprtHmEntr = tk.Entry(self.endFrame)
-		self.rsltLbl = tk.Label(self.endFrame, text = "Result of Story")
-		self.rsltEntr = tk.Entry(self.endFrame)
+		self.endFrame = tk.LabelFrame(self.root, text = "End Story", background = self.frameBckgrndClr, font = self.guiFrameFont, padx = self.framePadX, pady = self.framePadY)
+		self.trnsprtHmLbl = tk.Label(self.endFrame, text = "Method of Getting Home", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.trnsprtHmEntr = tk.Entry(self.endFrame, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.rsltLbl = tk.Label(self.endFrame, text = "Result of Story", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.rsltEntr = tk.Entry(self.endFrame, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.endFrame.grid(row = 6, column = 1)
 		self.trnsprtHmLbl.grid(row = 0, column = 0)
@@ -118,12 +129,12 @@ class Display:
 		self.rsltEntr.grid(row = 1, column = 1)
 
 		#Accessibility Frame
-		self.accFrame = tk.LabelFrame(self.root, text = "Accessibility")
-		self.hCLbl = tk.Label(self.accFrame, text = "High Contrast")
-		self.hCCBox = tk.Checkbutton(self.accFrame, text = "ON/OFF", variable = self.enableHighContrast)
-		self.fntSizeLbl = tk.Label(self.accFrame, text = "Font Size")
-		self.fntSizeSpBx = tk.Spinbox(self.accFrame, from_ = self.minFontSize, to = self.maxFontSize)
-		self.playTxtToSpchBttn = tk.Button(self.accFrame, text = "Play Text to Speech", command = self.bttnClicked)
+		self.accFrame = tk.LabelFrame(self.root, text = "Accessibility", background = self.frameBckgrndClr, font = self.guiFrameFont, padx = self.framePadX, pady = self.framePadY)
+		self.hCLbl = tk.Label(self.accFrame, text = "High Contrast", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.hCCBox = tk.Checkbutton(self.accFrame, text = "ON/OFF", variable = self.enableHighContrast, background = self.lblBgrndClr, font = self.guiLblFont)
+		self.fntSizeLbl = tk.Label(self.accFrame, text = "Font Size", background = self.lblBgrndClr, font = self.guiLblFont)
+		self.fntSizeSpBx = tk.Spinbox(self.accFrame, from_ = self.minFontSize, to = self.maxFontSize, background = self.entrBgrndClr, font = self.guiLblFont)
+		self.playTxtToSpchBttn = tk.Button(self.accFrame, text = "Play Text to Speech", command = self.bttnClicked, background = self.entrBgrndClr, font = self.guiLblFont)
 		#************ PACKING WIDGETS *****************************************#
 		self.accFrame.grid(row = 1, column = 0)
 		self.hCLbl.grid(row = 0, column = 0)
