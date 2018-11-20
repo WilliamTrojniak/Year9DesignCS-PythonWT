ca0b743b85b1ef9ac1fdf4493e70d1cbc10a61b8
William Trojniak <william.trojniak@Y22-Design-William-Trojniak.local>
2018-11-20 10:35:02 -0500

    Finished GUI layout

--- Project1/ProjectWT.py
+++ Project1/ProjectWT.py
@@ -32,7 +32,7 @@ class Display:
 		self.genreOMenu = tk.OptionMenu(self.storyFrame, self.genre, self.genreOptions[0], self.genreOptions[1], self.genreOptions[2])
 		self.strtPlaceEntr = tk.Entry(self.storyFrame)
 		#************ PACKING WIDGETS *****************************************#
-		self.storyFrame.grid(row = 1, column = 0)
+		self.storyFrame.grid(row = 1, column = 1)
 		self.genreLbl.grid(row = 0, column = 0)
 		self.strtPlaceLbl.grid(row = 0, column = 1)
 		self.genreOMenu.grid(row = 1, column = 0)
@@ -50,7 +50,7 @@ class Display:
 		self.heroStrngthEntr2 = tk.Entry(self.heroFrame)
 		self.heroStrngthEntr3 = tk.Entry(self.heroFrame)
 		#************ PACKING WIDGETS *****************************************#
-		self.heroFrame.grid(row = 2, column = 0)
+		self.heroFrame.grid(row = 2, column = 1)
 		self.heroNmLbl.grid(row = 0, column = 0, columnspan = 2)
 		self.heroNmEntr.grid(row = 1, column = 0, columnspan = 2)
 		self.heroGenderLbl.grid(row = 2, column = 0, columnspan = 2)
@@ -68,7 +68,7 @@ class Display:
 		self.mntrGftLbl = tk.Label(self.mntrFrame, text ="Mentor's Gift")
 		self.mntrGftEntr = tk.Entry(self.mntrFrame)
 		#************ PACKING WIDGETS *****************************************#
-		self.mntrFrame.grid(row = 3, column = 0)
+		self.mntrFrame.grid(row = 3, column = 1)
 		self.mntrNmLbl.grid(row = 0, column = 0)
 		self.mntrNmEntr.grid(row = 1, column = 0)
 		self.mntrGftLbl.grid(row = 0, column = 1)
@@ -83,7 +83,7 @@ class Display:
 		self.trialNmLbl = tk.Label(self.trialFrame, text = "Trial")
 		self.trialNmEntr = tk.Entry(self.trialFrame)
 		#************ PACKING WIDGETS *****************************************#
-		self.trialFrame.grid(row = 4, column = 0)
+		self.trialFrame.grid(row = 4, column = 1)
 		self.trialCntLbl.grid(row = 0, column = 1, columnspan = 2)
 		self.trialCntSpBx.grid(row = 1, column = 1, columnspan = 2)
 		self.trialLocLbl.grid(row = 2, column = 0, columnspan = 2)
@@ -98,7 +98,7 @@ class Display:
 		self.fChllngeNmLbl = tk.Label(self.fChllngeFrame, text = "Final Challenge")
 		self.fChllngeNmEntr = tk.Entry(self.fChllngeFrame)
 		#************ PACKING WIDGETS *****************************************#
-		self.fChllngeFrame.grid(row = 5, column = 0)
+		self.fChllngeFrame.grid(row = 5, column = 1)
 		self.fChllngeLocLbl.grid(row = 0, column = 0)
 		self.fChllngeLocEntr.grid(row = 1, column = 0)
 		self.fChllngeNmLbl.grid(row = 0, column = 1)
@@ -111,7 +111,7 @@ class Display:
 		self.rsltLbl = tk.Label(self.endFrame, text = "Result of Story")
 		self.rsltEntr = tk.Entry(self.endFrame)
 		#************ PACKING WIDGETS *****************************************#
-		self.endFrame.grid(row = 6, column = 0)
+		self.endFrame.grid(row = 6, column = 1)
 		self.trnsprtHmLbl.grid(row = 0, column = 0)
 		self.trnsprtHmEntr.grid(row = 1, column = 0)
 		self.rsltLbl.grid(row = 0, column = 1)
@@ -120,12 +120,12 @@ class Display:
 		#Accessibility Frame
 		self.accFrame = tk.LabelFrame(self.root, text = "Accessibility")
 		self.hCLbl = tk.Label(self.accFrame, text = "High Contrast")
-		self.hCCBox = tk.Checkbutton(self.accFrame, text = "ON / OFF", variable = self.enableHighContrast)
+		self.hCCBox = tk.Checkbutton(self.accFrame, text = "ON/OFF", variable = self.enableHighContrast)
 		self.fntSizeLbl = tk.Label(self.accFrame, text = "Font Size")
 		self.fntSizeSpBx = tk.Spinbox(self.accFrame, from_ = self.minFontSize, to = self.maxFontSize)
 		self.playTxtToSpchBttn = tk.Button(self.accFrame, text = "Play Text to Speech", command = self.bttnClicked)
 		#************ PACKING WIDGETS *****************************************#
-		self.accFrame.grid(row = 1, column = 2)
+		self.accFrame.grid(row = 1, column = 0)
 		self.hCLbl.grid(row = 0, column = 0)
 		self.hCCBox.grid(row = 1, column = 0)
 		self.fntSizeLbl.grid(row = 0, column = 1)
@@ -133,7 +133,13 @@ class Display:
 		self.playTxtToSpchBttn.grid(row = 2, column = 0, columnspan = 2)
 
 		#Buttons and Output Text
-		
+		self.genStoryBttn = tk.Button(self.root, text = "Generate Story", command = self.bttnClicked)
+		self.outTxt = tk.Text(self.root, height = 8, width = 50)
+		self.saveBttn = tk.Button(self.root, text = "Save", command = self.bttnClicked)
+		#************ PACKING WIDGETS *****************************************#
+		self.genStoryBttn.grid(row = 7, column = 1)
+		self.outTxt.grid(row = 8, column = 1)
+		self.saveBttn.grid(row = 9, column = 1)
 
 
 
